901ef72624b62ccc6b905291043bdf2de1bb471e
minseo <minseo9912@naver.com>
2021-08-21 03:33:51 +0900

    3.3 반영

--- src/App.js
+++ src/App.js
@@ -2,30 +2,19 @@ import React from "react";
 import PropTypes from "prop-types";
 
 class App extends React.Component{
-  // 바꾸고 싶은 내용을 state에 기입
   state = {
-    count: 0
-  };
-  add = () => {
-    this.setState(current => ({count: current.count + 1}));
-  };
-  minus = () => {
-    this.setState(current => ({count: current.count - 1}));
+    isLoading: true,
+    movies: []
   };
   componentDidMount(){
-    console.log("component rendered");
-  };
-  componentDidUpdate(){
-    console.log("I just updated");
-  };
+    setTimeout(() => {
+      this.setState({ isLoading : false });
+    }, 6000);
+  }
   render(){
-    console.log("I'm rendering");
-    return (
-      <div>
-        <h1>The number is : {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
+    const { isLoading } = this.state;
+    return (      
+      <div>{ isLoading ? "Loading..." : "We are ready"}</div>
     );
   }
 }
